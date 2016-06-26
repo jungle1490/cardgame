f78149cde8877bf8ac19a8791fa9cb73d3a39ce0
joejungle <jungle1490@gmail.com>
2016-06-26 15:42:51 +0800

    move character's skill into their class

---
+++ app/src/main/java/com/example/joejungle/game/ViewController/battle/Battle.java
@@ -0,0 +1,15 @@
+package com.example.joejungle.game.ViewController.battle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.joejungle.game.R;
+
+public class Battle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battle);
+    }
+}
--- app/src/main/java/com/example/joejungle/game/ViewController/newCharacter/SelectCharacter.java
+++ app/src/main/java/com/example/joejungle/game/ViewController/newCharacter/SelectCharacter.java
@@ -2,6 +2,7 @@ package com.example.joejungle.game.ViewController.newCharacter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.joejungle.game.R;
 
@@ -11,5 +12,15 @@ public class SelectCharacter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_character);
+
+
+    }
+
+    protected void KronosSelect(View view){
+
+    }
+
+    protected void UranusSelect(View view ){
+
     }
 }
